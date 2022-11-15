94fa4e223b0376904a0042b45971e53711fd77a3
kartavyagupta <kartavyagupta96@gmail.com>
2022-11-15 20:36:07 +0530

    added tinymce editor

--- icoder/blog/admin.py
+++ icoder/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import post,comment
 #Register your models here.
-admin.site.register((post,comment))
+admin.site.register((comment))
+@admin.register(post)
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('tinyInject.js',)
